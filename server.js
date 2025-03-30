0a7589d560433cc06df98a2199df041bc26c92be
Iris van Willigen <iris.van.willigen@gmail.com>
2025-03-30 16:19:58 +0200

    Add routes for gardening

--- server.js
+++ server.js
@@ -13,11 +13,39 @@ const engine = new Liquid();
 app.engine('liquid', engine.express());
 app.set('views', './views');
 
-app.get('/', async function (req, res) {
-    const markdownContent = fs.readFileSync('../i-love-web/posts/2025-03-25.md', 'utf8');
-    const testContent = md.render(markdownContent);
-    res.render('index.liquid', { content: testContent});
+// app.get('/', async function (req, res) {
+//     const files = fs.readdirSync('../i-love-data-driven-web/posts');
+//     console.log(files);
+//     res.render('index.liquid', { 
+//         content: testContent,
+//         links: files
+//     });
+// });
+
+app.get('/home', async function (req, res) {
+    const files = fs.readdirSync('../i-love-data-driven-web/posts');
+
+    res.render('index.liquid', { 
+        links: files,
+    });
+});
+
+app.get('/garden', async function (req, res) {
+    const files = fs.readdirSync('../i-love-data-driven-web/garden');
+
+    res.render('index.liquid', {
+        links: files,
+    })
 })
+
+app.get('/post/:file', async function (req, res) {
+    const file = req.params.file;
+    const markdownContent = fs.readFileSync(`../i-love-data-driven-web/posts/${file}`, 'utf8');
+    const testContent = md.render(markdownContent);
+    res.render('post.liquid', { 
+        content: testContent,
+    });
+});
 
 app.set('port', process.env.PORT || 8000);
 
